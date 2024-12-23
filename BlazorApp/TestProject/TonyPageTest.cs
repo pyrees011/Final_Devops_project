6685cd5f5017178b1def17e83b1777a57c679fe4
Pyrees011 <raunak.bhansali17@gmail.com>
2024-12-24 00:38:54 +0100

    fix: /tony route adding the embeded video

--- BlazorApp/TestProject/TonyPageTest.cs
+++ BlazorApp/TestProject/TonyPageTest.cs
@@ -13,9 +13,22 @@ public class TonyPageTest: Bunit.TestContext
         var component = RenderComponent<BlazorApp.Components.Pages.Tony>();
 
         // Assert
-        component.MarkupMatches(@"          
-            <h1>Hello, Tony</h1>
-            Welcome to our blazor app.
+        component.MarkupMatches(@"
+            <h1>Welcome to Tony's Page</h1>
+            <p>Here is an embedded video:</p>
+            <div style=""position:relative; padding-bottom:56.25%; height:0; overflow:hidden;"">
+                <iframe 
+                    width=""560"" 
+                    height=""315"" 
+                    src=""https://www.youtube.com/embed/aAkMkVFwAoo?si=JFRDnFiIjHkynbRY"" 
+                    title=""YouTube video player"" 
+                    frameborder=""0"" 
+                    style=""position:absolute; top:0; left:0; width:100%; height:100%;"" 
+                    allow=""accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"" 
+                    referrerpolicy=""strict-origin-when-cross-origin"" 
+                    allowfullscreen>
+                </iframe>
+            </div>
         ");
     }
 }
